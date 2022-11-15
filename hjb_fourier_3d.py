FIX: Range normalize data, generate 2^5 resolution data. TODO: clip terminal state range. Determine axis for normalization. Sample xf from Gaussian

hjb_fourier_3d.py
@@ -169,7 +169,7 @@ class FNO3d(nn.Module):
 # TEST_PATH = TRAIN_PATH
 
 # Dubins car value function data
-TRAIN_PATH = 'data/N1100_R4.mat'
+TRAIN_PATH = 'data/N1100_R5.mat'
 TEST_PATH = TRAIN_PATH
 
 
@@ -201,7 +201,7 @@ path_image = 'image/'+path
 runtime = np.zeros(2, )
 t1 = default_timer()
 
-grid_res = 16
+grid_res = 32
 sub = 1
 S = grid_res // sub
 
@@ -222,12 +222,15 @@ assert (S == train_u.shape[-2])
 assert (S == train_u.shape[-1])
 
 
-a_normalizer = UnitGaussianNormalizer(train_a)
+# a_normalizer = UnitGaussianNormalizer(train_a)
+a_normalizer = RangeNormalizer(train_a)
 train_a = a_normalizer.encode(train_a)
+a_normalizer = RangeNormalizer(test_a)
 test_a = a_normalizer.encode(test_a)
 
-y_normalizer = UnitGaussianNormalizer(train_u)
-train_u = y_normalizer.encode(train_u)
+# y_normalizer = UnitGaussianNormalizer(train_u)
+# y_normalizer = RangeNormalizer(train_a)
+# train_u = y_normalizer.encode(train_u)
 
 train_a = train_a.reshape(ntrain,S,S,S,1)
 test_a = test_a.reshape(ntest,S,S,S,1)
@@ -246,13 +249,13 @@ device = torch.device('cuda')
 model = FNO3d(modes, modes, modes, width).cuda()
 # model = torch.load('model/ns_fourier_V100_N1000_ep100_m8_w20')
 
-print(count_params(model))
+print("Number of model parameters: ", count_params(model))
 optimizer = Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
 
 
 myloss = LpLoss(size_average=False)
-y_normalizer.cuda()
+# y_normalizer.cuda()
 for ep in range(epochs):
     model.train()
     t1 = default_timer()
@@ -267,8 +270,8 @@ for ep in range(epochs):
         mse = F.mse_loss(out, y, reduction='mean')
         # mse.backward()
 
-        y = y_normalizer.decode(y)
-        out = y_normalizer.decode(out)
+        # y = y_normalizer.decode(y)
+        # out = y_normalizer.decode(out)
         l2 = myloss(out.view(batch_size, -1), y.view(batch_size, -1))
         l2.backward()
 
@@ -285,7 +288,7 @@ for ep in range(epochs):
             x, y = x.cuda(), y.cuda()
 
             out = model(x).view(batch_size, S, S, S)
-            out = y_normalizer.decode(out)
+            # out = y_normalizer.decode(out)
             test_l2 += myloss(out.view(batch_size, -1), y.view(batch_size, -1)).item()
 
     train_mse /= len(train_loader)
@@ -310,7 +313,7 @@ with torch.no_grad():
         # print("out shape: ", out.shape)
         # print("mean shape: ", y_normalizer.mean.shape)
         # print("std shape: ", y_normalizer.std.shape)
-        out = y_normalizer.decode(out)
+        # out = y_normalizer.decode(out)
         pred[index] = out
 
         test_l2 += myloss(out.view(1, -1), y.view(1, -1)).item()

utilities3.py
@@ -135,18 +135,36 @@ class GaussianNormalizer(object):
 
 # normalization, scaling by range
 class RangeNormalizer(object):
-    def __init__(self, x, low=0.0, high=1.0):
+    def __init__(self, x, low=0.0, high=1.0, eps=0.00001):
         super(RangeNormalizer, self).__init__()
-        mymin = torch.min(x, 0)[0].view(-1)
-        mymax = torch.max(x, 0)[0].view(-1)
 
+        mymin = x.reshape(x.size()[0], -1).min(axis=-1)[0]
+        mymax = x.reshape(x.size()[0], -1).max(axis=-1)[0]
+        # mymin = torch.min(x, 0)[0].view(-1)
+        # mymax = torch.max(x, 0)[0].view(-1)
+        print("mymin: ", mymin)
+        print("mymax: ", mymax)
+        print(mymin.size())
+        print(mymax.size())
+        # self.eps = eps
         self.a = (high - low)/(mymax - mymin)
         self.b = -self.a*mymax + high
+        # self.b = -self.a * mymin
+
+        print("a: ", self.a)
+        print("b: ", self.b)
+        self.a = self.a.view(self.a.size()[0], 1, 1, 1)
+        self.b = self.b.view(self.b.size()[0], 1, 1, 1)
 
     def encode(self, x):
         s = x.size()
-        x = x.view(s[0], -1)
+        print("size of x: ", s)
+        print("size of a: ", self.a.size())
+        print("size of b: ", self.b.size())
+
+        print("size of a*x: ", torch.mul(self.a,x))
         x = self.a*x + self.b
+        print("normalized x: ", x)
         x = x.view(s)
         return x
 
@@ -156,6 +174,10 @@ class RangeNormalizer(object):
         x = (x - self.b)/self.a
         x = x.view(s)
         return x
+
+    def cuda(self):
+        self.a = self.a.cuda()
+        self.b = self.b.cuda()
 
 #loss function with rel/abs Lp loss
 class LpLoss(object):